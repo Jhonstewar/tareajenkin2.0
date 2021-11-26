0e4723e488e5c70312e665064888315c46dfa09f
Jhonstewar <jhonstewarnunezcastellanos@gmail.com>
2021-11-26 17:01:28 -0500

    carga

---
+++ Jenkinsfile
@@ -0,0 +1,29 @@
+pipeline {
+    agent any
+
+    tools {
+        // Install the Maven version configured as "M3" and add it to the path.
+        maven "Maven 3.8.3"
+    }
+
+    stages {
+        
+        
+        stage('Run') {
+            steps {
+
+                //bat 'mvn spring-boot:run'
+                echo 'Servidor Activo'
+            }
+        }
+         stage('getMoto Contructor') {
+            steps {
+
+                bat 'localhost:8081/getMoto?marca=Bajaj'
+                echo 'Petición Get con filtro por marca (Bajaj)'
+             }
+        }
+                
+       
+    }   
+}
---
+++ src/main/java/co/edu/uis/GreetingController.java
@@ -0,0 +1,153 @@
+package co.edu.uis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+//@RequestMapping("/demo")
+public class GreetingController {
+
+	private static ArrayList<String> moto;
+	private static List<ArrayList<String>> motos = new ArrayList<>();
+	private static ArrayList<Greeting> motos2;
+
+	public GreetingController(){
+		moto = new ArrayList<String>();
+
+		moto.add("01");		moto.add("Honda");		moto.add("Cb 110");		moto.add("2018");		moto.add("Azul");
+
+		motos.add(moto);
+
+		moto = new ArrayList<String>();
+
+		moto.add("02");		moto.add("Bajaj");		moto.add("Pulsar 150");		moto.add("2015");		moto.add("negra");
+
+		motos.add(moto);
+		moto = new ArrayList<String>();
+
+		moto.add("03");		moto.add("Suzuki");		moto.add("Gs 150");		moto.add("2020");		moto.add("roja");
+
+		motos.add(moto);
+		moto = new ArrayList<String>();
+
+		moto.add("04");		moto.add("Bajaj");		moto.add("Boxer ct 100");		moto.add("2019");		moto.add("gris");
+
+		motos.add(moto);
+	}
+	
+
+	@GetMapping("/getMoto")
+	public ArrayList<Greeting> greeting(@RequestParam(value = "id", defaultValue = "null") String id,
+											@RequestParam(value = "marca", defaultValue = "null") String marca,
+											@RequestParam(value = "nombre", defaultValue = "null") String nombre,
+											@RequestParam(value = "year", defaultValue = "null") String year,
+											@RequestParam(value = "color", defaultValue = "null") String color) {
+	motos2 = new ArrayList<>();
+			if (!id.equals("null")) {
+				for(ArrayList<String> moto: motos){
+					if (moto.contains(id)) {
+						motos2.add(new Greeting(moto.get(0), moto.get(1), moto.get(2), moto.get(3), moto.get(4)));
+					}
+				}
+				return motos2;
+			}
+			if (!marca.equals("null")) {
+				for(ArrayList<String> moto: motos){
+					if (moto.contains(marca)) {
+						motos2.add(new Greeting(moto.get(0), moto.get(1), moto.get(2), moto.get(3), moto.get(4)));
+					}
+				}
+				return motos2;
+			}
+			if (!nombre.equals("null")) {
+				for(ArrayList<String> moto: motos){
+					if (moto.contains(nombre)) {
+						motos2.add(new Greeting(moto.get(0), moto.get(1), moto.get(2), moto.get(3), moto.get(4)));
+					}
+				}
+				return motos2;
+			}
+			if (!year.equals("null")) {
+				for(ArrayList<String> moto: motos){
+					if (moto.contains(year)) {
+						motos2.add(new Greeting(moto.get(0), moto.get(1), moto.get(2), moto.get(3), moto.get(4)));
+					}
+				}
+				return motos2;
+			}
+			if (!color.equals("null")) {
+				for(ArrayList<String> moto: motos){
+					if (moto.contains(color)) {
+						motos2.add(new Greeting(moto.get(0), moto.get(1), moto.get(2), moto.get(3), moto.get(4)));
+					}
+				}
+				return motos2;
+			}
+			for(ArrayList<String> moto: motos){
+				motos2.add(new Greeting(moto.get(0), moto.get(1), moto.get(2), moto.get(3), moto.get(4)));
+			}
+			return motos2;
+	}
+
+	// aqui se crean las motos
+
+	@PostMapping("/addMoto")
+	public void registrar(@RequestParam String id,
+						@RequestParam String marca,
+						@RequestParam String nombre,
+						@RequestParam String year,
+						@RequestParam String color){
+
+		moto = new ArrayList<String>();
+
+		moto.add(id);
+		moto.add(marca);
+		moto.add(nombre);
+		moto.add(year);
+		moto.add(color);
+
+		motos.add(moto);
+	}
+
+	@PutMapping("/editMoto")
+	public void actualizar(@RequestParam String id,
+	@RequestParam String marca,
+	@RequestParam String nombre,
+	@RequestParam String year,
+	@RequestParam String color)
+	{
+		
+		for(ArrayList<String> moto: motos){
+			if (moto.contains(id)) {
+				moto.set(1,marca);
+				moto.set(2,nombre);
+				moto.set(3,year);
+				moto.set(4,color);
+			}
+			
+		}
+
+	}
+
+	@DeleteMapping("/deleteMoto")
+	public void deletear(@RequestParam String id)
+	{
+		int i=0;
+		for(Greeting moto: motos2){
+			if (moto.getId().equals(id)) {
+				motos.remove(i);
+			}
+			i++;
+			
+		}
+	}
+}
